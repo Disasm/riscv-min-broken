ceed0ad18e0275c25568f2b4428ba0043fb51346
Vadim Kaushan <admin@disasm.info>
2019-07-19 16:09:08 +0300

    Add more variables

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,12 @@ extern "C" {
     fn consume(value: usize);
 }
 
-static mut BYTE: u8 = 1;
+#[link_section = ".data"]
+static mut BYTE_DATA: u8 = 1;
 #[link_section = ".rodata"]
-static mut BYTE_RODATA: u8 = 1;
+static BYTE_RODATA: u8 = 1;
+#[link_section = ".bss"]
+static mut BYTE_BSS: u8 = 0;
 
 #[link_section = ".init.rust"]
 #[export_name = "_start_rust"]
@@ -22,8 +25,9 @@ pub extern "C" fn start_rust() -> ! {
     unsafe {
         let ptr = consume as *const fn(usize) as usize;
         consume(ptr);
-        consume(BYTE as usize);
+        consume(BYTE_DATA as usize);
         consume(BYTE_RODATA as usize);
+        consume(BYTE_BSS as usize);
     }
 
     loop {}
